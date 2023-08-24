1004a0f821a94614c27689c5f78be3e37ecf79e2
parkjikuk <paxkk@naver.com>
2023-08-24 16:42:36 +0900

    router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import { BrowserRouter,Route, Routes } from 'react-router-dom';
+import Terms from './pages/Terms';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/terms" element={<Terms />}></Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
