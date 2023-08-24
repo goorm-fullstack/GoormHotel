c756ccf85da9c94341644292764716ccbf8e35f8
parkjikuk <paxkk@naver.com>
2023-08-24 16:44:55 +0900

    pages component folder

---
+++ frontend/src/pages/Terms.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { styled } from 'styled-components';
+import Header from '../components/Header';
+
+const Container = styled.div`
+  min-height: 100vh;
+  
+`;
+
+const Terms = () => {
+  return (
+    <>
+      <Header />
+      <Container>
+        
+      </Container>
+    </>
+  );
+};
+
+export default Terms;
