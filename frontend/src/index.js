c131e405be0baa1e65c36100cdac8c56089fed16
parkjikuk <paxkk@naver.com>
2023-08-24 16:44:06 +0900

    Global style component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import GlobalStyle from './styles/GlobalStyles';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={ theme }>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>
+  </ThemeProvider>
 );
 
