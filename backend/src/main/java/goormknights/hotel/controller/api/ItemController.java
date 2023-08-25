d6243ac3181f41214ae0fb3bc471f557a46ac430
JinhwanB <paewhal20@gmail.com>
2023-08-25 19:04:45 +0900

    검색 기능 구현

--- backend/src/main/java/goormknights/hotel/controller/api/ItemController.java
+++ backend/src/main/java/goormknights/hotel/controller/api/ItemController.java
@@ -4,10 +4,12 @@ import goormknights.hotel.model.Dining;
 import goormknights.hotel.model.Item;
 import goormknights.hotel.model.Room;
 import goormknights.hotel.service.ItemService;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -24,6 +26,27 @@ public class ItemController {
     @GetMapping("/items")
     public ResponseEntity<List<Object>> allItems(){
         List<Item> allItem = itemService.findAllItem();
+        List<Object> responseItem = getResponseItem(allItem);
+
+        return ResponseEntity.ok(responseItem);
+    }
+
+    /**
+     * 상품 검색 기능
+     * @param keyword - 클라이언트가 검색한 단어
+     * @return 검색 단어가 들어가 있는 상품명을 찾아서 조회
+     */
+    @GetMapping("/search")
+    public ResponseEntity<List<Object>> findItems(@RequestParam String keyword){
+        log.info(keyword);
+        List<Item> searchResult = itemService.findByKeyword(keyword);
+        List<Object> responseItem = getResponseItem(searchResult);
+
+        return ResponseEntity.ok(responseItem);
+    }
+
+    @NotNull
+    private static List<Object> getResponseItem(List<Item> allItem) {
         List<Object> responseItem = new ArrayList<>();
 
         for (Item item : allItem) {
@@ -37,7 +60,6 @@ public class ItemController {
                 log.info("responseRoom={}", item);
             }
         }
-
-        return ResponseEntity.ok(responseItem);
+        return responseItem;
     }
 }
