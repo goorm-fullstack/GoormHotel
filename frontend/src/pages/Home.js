573df9509b8576a3df910573d2fe7dda7f217ca5
JinhwanB <paewhal20@gmail.com>
2023-08-28 02:42:04 +0900

    modify

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,19 @@
+import { styled } from "styled-components";
+
+export const DetailBtn = styled.button`
+  font-size: 15px;
+  padding: 15px 20px;
+  background-color: #95846e;
+  color: white;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const DetailSvg = styled.svg`
+  width: 15px;
+  height: 15px;
+  margin-left: 25px;
+  fill: white;
+`;
