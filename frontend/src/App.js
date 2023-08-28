76dfc6a7f4c09eced958d2720ae40a2b318ec4ae
WhiteKIM <9rudrb40@naver.com>
2023-08-28 22:29:54 +0900

    2023.08.28 - [FE] upload
    개인정보, 이용약관 페이지 업로드

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import Home from './pages/Home';
 import { BrowserRouter,Route, Routes } from 'react-router-dom';
 import Terms from './pages/Terms';
 import Way from './pages/way/Way';
+import Agreement from './pages/register/Agreement';
+import Privacy from './pages/register/Privacy';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/terms" element={<Terms />}></Route>
         <Route path="/location" element={<Way />}></Route>
+        <Route path="/agreement" element={<Agreement />}></Route>
+        <Route path="/privacy" element={<Privacy />}></Route>
       </Routes>
       <Footer />
     </BrowserRouter>
