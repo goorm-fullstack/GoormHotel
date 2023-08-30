1ffca5cbccc8e744537453408edf83d0349846c3
Sohee Moon <soheetech@gmail.com>
2023-08-30 17:27:27 +0900

    초기화

--- backend/src/main/java/goormknights/hotel/HotelApplication.java
+++ backend/src/main/java/goormknights/hotel/HotelApplication.java
@@ -2,13 +2,8 @@ package goormknights.hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableJpaAuditing
 @SpringBootApplication
-@EnableScheduling
-@EnableJpaAuditing
 public class HotelApplication {
 
 	public static void main(String[] args) {
