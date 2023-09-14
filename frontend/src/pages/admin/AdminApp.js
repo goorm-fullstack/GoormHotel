63d9dd518abbe011ba0d6e49e03cf7755a4d8046
Sohee Moon <soheetech@gmail.com>
2023-09-14 20:15:24 +0900

    admin 채팅/메일 페이지 작업 완료

--- frontend/src/pages/admin/AdminApp.js
+++ frontend/src/pages/admin/AdminApp.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AdminLogin from './AdminLogin';
 import AdminMember from './AdminMember';
 import AdminMemberDetail from './AdminMemberDetail';
+import AdminChat from './chat/AdminChat';
+import AdminChatDetail from './chat/AdminChatDetail';
+import AdminMail from './chat/AdminMail';
 
 const AdminApp = () => {
   return (
@@ -11,6 +14,9 @@ const AdminApp = () => {
         <Route path="/admin/login" element={<AdminLogin />}></Route>
         <Route path="/admin/member" element={<AdminMember />}></Route>
         <Route path="/admin/member/:memberId" element={<AdminMemberDetail />}></Route>
+        <Route path="/admin/chat" element={<AdminChat />}></Route>
+        <Route path="/admin/chat/:memberId" element={<AdminChatDetail />}></Route>
+        <Route path="/admin/mail" element={<AdminMail />}></Route>
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/pages/admin/AdminHeader.js
+++ frontend/src/pages/admin/AdminHeader.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import adminLogo from '../../images/common/logo_admin.png'
+import adminLogo from '../../images/common/logo_admin.png';
 import { Link } from 'react-router-dom';
 
 const Container = styled.div`
@@ -10,7 +10,7 @@ const Container = styled.div`
 const Wrapper = styled.div`
   width: 100%;
   height: 97px;
-  background-color: #21201E;
+  background-color: #21201e;
   position: fixed;
   display: flex;
   align-items: center;
@@ -25,11 +25,11 @@ const HeaderLink = styled(Link)`
 const MenuItem = styled.ul`
   display: flex;
   gap: 40px;
-  color: #FFFFFF;
+  color: #ffffff;
   flex: 1;
-  
+
   li:hover {
-    color: #BAA085;
+    color: #baa085;
   }
 `;
 
@@ -40,17 +40,17 @@ const HeaderRight = styled.div`
 `;
 
 const ManagerId = styled.span`
-  color: #FFFFFF;
+  color: #ffffff;
   margin-right: 15px;
   margin-left: 10px;
 `;
 
 const LogoutBtn = styled.button`
-  border: 1px solid #FFFFFF;
+  border: 1px solid #ffffff;
   border-radius: 50px;
   padding: 8px 0;
   background-color: transparent;
-  color: #FFFFFF;
+  color: #ffffff;
   width: 90px;
 `;
 
@@ -58,32 +58,43 @@ const AdminHeader = () => {
   return (
     <Container>
       <Wrapper>
-        <HeaderLink to="/admin/member"><img src={adminLogo} alt="logo" /></HeaderLink>
+        <HeaderLink to="/admin/member">
+          <img src={adminLogo} alt="logo" />
+        </HeaderLink>
         <MenuItem>
-          <li><Link to="/admin/member">회원 관리</Link></li>
-          <li><Link>상품 관리</Link></li>
-          <li><Link>예약 관리</Link></li>
-          <li><Link>게시판 관리</Link></li>
-          <li><Link>채팅/메일 관리</Link></li>
+          <li>
+            <Link to="/admin/member">회원 관리</Link>
+          </li>
+          <li>
+            <Link>상품 관리</Link>
+          </li>
+          <li>
+            <Link>예약 관리</Link>
+          </li>
+          <li>
+            <Link>게시판 관리</Link>
+          </li>
+          <li>
+            <Link to="/admin/chat">채팅/메일 관리</Link>
+          </li>
         </MenuItem>
         <HeaderRight>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          version="1.1"
-          viewBox="0 0 24 24"
-          style={{ enableBackground: 'new 0 0 24 24', width: '28px', height: '28px', fill: '#DDDDDD' }}
-          xmlSpace="preserve"
-        >
-          <g id="info" />
-          <g id="icons">
-            <path
-              d="M12,0C5.4,0,0,5.4,0,12c0,6.6,5.4,12,12,12s12-5.4,12-12C24,5.4,18.6,0,12,0z M12,4c2.2,0,4,2.2,4,5s-1.8,5-4,5   s-4-2.2-4-5S9.8,4,12,4z M18.6,19.5C16.9,21,14.5,22,12,22s-4.9-1-6.6-2.5c-0.4-0.4-0.5-1-0.1-1.4c1.1-1.3,2.6-2.2,4.2-2.7   c0.8,0.4,1.6,0.6,2.5,0.6s1.7-0.2,2.5-0.6c1.7,0.5,3.1,1.4,4.2,2.7C19.1,18.5,19.1,19.1,18.6,19.5z"
-              id="user2"
-            />
-          </g>
-        </svg>
-        <ManagerId>관리자(memberId)</ManagerId>
-        <LogoutBtn>로그아웃</LogoutBtn>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            version="1.1"
+            viewBox="0 0 24 24"
+            style={{ enableBackground: 'new 0 0 24 24', width: '28px', height: '28px', fill: '#DDDDDD' }}
+            xmlSpace="preserve">
+            <g id="info" />
+            <g id="icons">
+              <path
+                d="M12,0C5.4,0,0,5.4,0,12c0,6.6,5.4,12,12,12s12-5.4,12-12C24,5.4,18.6,0,12,0z M12,4c2.2,0,4,2.2,4,5s-1.8,5-4,5   s-4-2.2-4-5S9.8,4,12,4z M18.6,19.5C16.9,21,14.5,22,12,22s-4.9-1-6.6-2.5c-0.4-0.4-0.5-1-0.1-1.4c1.1-1.3,2.6-2.2,4.2-2.7   c0.8,0.4,1.6,0.6,2.5,0.6s1.7-0.2,2.5-0.6c1.7,0.5,3.1,1.4,4.2,2.7C19.1,18.5,19.1,19.1,18.6,19.5z"
+                id="user2"
+              />
+            </g>
+          </svg>
+          <ManagerId>관리자(memberId)</ManagerId>
+          <LogoutBtn>로그아웃</LogoutBtn>
         </HeaderRight>
       </Wrapper>
     </Container>
