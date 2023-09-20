162083ba514f1baf6a57f07ee76e4ef03e33fbff
Memil <50459192+yss1902@users.noreply.github.com>
2023-09-20 14:47:10 +0900

    commit

--- backend/src/main/java/goormknights/hotel/auth/config/filter/EmailPasswordAuthFilter.java
+++ backend/src/main/java/goormknights/hotel/auth/config/filter/EmailPasswordAuthFilter.java
@@ -1,17 +1,21 @@
 package goormknights.hotel.auth.config.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 
 import java.io.IOException;
 
+@Slf4j
 public class EmailPasswordAuthFilter extends AbstractAuthenticationProcessingFilter {
 
     private final ObjectMapper objectMapper;
@@ -30,6 +34,15 @@ public class EmailPasswordAuthFilter extends AbstractAuthenticationProcessingFil
 
         token.setDetails(this.authenticationDetailsSource.buildDetails(request));
         return this.getAuthenticationManager().authenticate(token);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        // 인증이 성공한 경우, SecurityContextHolder에 인증 정보를 설정합니다.
+        SecurityContextHolder.getContext().setAuthentication(authResult);
+        log.info("Authentication success : {}", authResult.toString());
+        // 다음 필터로 전달
+        chain.doFilter(request, response);
     }
 
     @Getter
