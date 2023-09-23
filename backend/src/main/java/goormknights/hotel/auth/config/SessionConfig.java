f59f44cb0b0c322f7be41f4feb824257f58e4233
Memil <50459192+yss1902@users.noreply.github.com>
2023-09-24 05:59:40 +0900

    commit

---
+++ backend/src/main/java/goormknights/hotel/auth/config/SessionConfig.java
@@ -0,0 +1,9 @@
+package goormknights.hotel.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+
+@Configuration
+@EnableJdbcHttpSession
+public class SessionConfig {
+}
