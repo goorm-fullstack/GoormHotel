8a08e0304d2233e9255edf315831119818317c9a
WhiteKIM <9rudrb40@naver.com>
2023-09-30 13:14:00 +0900

    2023.09.30 upload

--- backend/src/main/java/goormknights/hotel/global/config/WebConfig.java
+++ backend/src/main/java/goormknights/hotel/global/config/WebConfig.java
@@ -1,8 +1,10 @@
 package goormknights.hotel.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -12,6 +14,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${resource.handler}")
+    private String resourceHandler;
+
+    @Value("${resource.location}")
+    private String resourceLocation;
+
     /**
      * Cors 에러 사용
      */
@@ -22,5 +31,12 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(resourceHandler)
+                .addResourceLocations(resourceLocation);
+
     }
 }
--- backend/src/main/java/goormknights/hotel/global/controller/CKEditorController.java
+++ backend/src/main/java/goormknights/hotel/global/controller/CKEditorController.java
@@ -5,9 +5,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.apache.commons.codec.binary.Base64;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 /**
  * 리액트 CKEditor에서 처리가 필요한 로직이 둘어있습니다.
@@ -19,18 +21,23 @@ public class CKEditorController {
     @Value("${file.upload}")
     private String path;
 
+    private String location = "/mail/upload/img/";
+
     @PostMapping("/upload/img")
     public ResponseEntity<String> imgUpload(@RequestPart(name = "file", required = false) MultipartFile file) {
         try {
             // 업로드된 파일을 지정된 디렉토리에 저장
             String fileName = file.getOriginalFilename();
-            File destinationFile = new File(path + File.separator + fileName);
+            String newFileName = UUID.randomUUID().toString()+fileName;
+            File destinationFile = new File(path + newFileName);
             file.transferTo(destinationFile);
 
-            // 여기서 파일 정보를 데이터베이스에 저장할 수 있음 (옵션)
+            //바이트 단위로 이미지를 디코딩
+            byte[] bytes = java.nio.file.Files.readAllBytes(destinationFile.toPath());
+            String base64Image = Base64.encodeBase64String(bytes);
 
             // 클라이언트에게 업로드 성공 메시지 전송
-            return ResponseEntity.ok(destinationFile.getAbsolutePath());
+            return ResponseEntity.ok("data:image/png;base64,"+base64Image);
         } catch (IOException e) {
             e.printStackTrace();
             // 업로드 실패 시 에러 응답
