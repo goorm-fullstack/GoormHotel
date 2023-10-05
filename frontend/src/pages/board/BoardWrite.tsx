1a2db2b978a1f9165e5fca17efbd197d5d46ea95
LEE-Donggyu <ehdrb141@naver.com>
2023-10-06 06:22:24 +0900

    subHeader map(nav,index)으로 수정 boardList 최대한 수정

--- frontend/src/pages/board/BoardWrite.tsx
+++ frontend/src/pages/board/BoardWrite.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react';
 import { styled } from 'styled-components';
-import { useParams, useNavigate } from 'react-router-dom'; // useHistory 추가
+import { useParams, useNavigate } from 'react-router-dom';
 import { commonContainerStyle, PageTitle, BtnWrapper, SubmitBtn, commonTable } from '../../Style/commonStyles';
 import SubHeader from '../../components/layout/SubHeader/SubHeader';
 import axios from 'axios';
@@ -39,7 +39,8 @@ const BoardWrite = () => {
     category: '',
   });
 
-  //이미지 업로드 input의 onChange
+  const [categoryError, setCategoryError] = useState<string>('');
+
   const saveImgFile = () => {
     const file = imgRef.current && imgRef.current.files ? imgRef.current.files[0] : '';
     const reader = new FileReader();
@@ -49,124 +50,132 @@ const BoardWrite = () => {
     };
   };
 
-  //input 입력 시
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
       [name]: value,
     });
+
+    if (name === 'category') {
+      setCategoryError('');
+    }
   };
 
-  //게시글 작성 api
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
+    e.preventDefault();
 
-      const form = new FormData();
-      form.append('multipartFile', imgRef.current && imgRef.current.files ? imgRef.current.files[0] : '');
-      formData.boardContent = boardContent;
+    if (!formData.category) {
+      setCategoryError('카테고리를 선택하세요'); // 오류 메시지 설정
+      return;
+    }
 
-      Object.keys(formData).forEach((key) => {
-        // Object.keys 수정
-        form.append(key, formData[key]);
+    const form = new FormData();
+    form.append('multipartFile', imgRef.current && imgRef.current.files ? imgRef.current.files[0] : '');
+    formData.boardContent = boardContent;
+
+    Object.keys(formData).forEach((key) => {
+      form.append(key, formData[key]);
+    });
+
+    try {
+      await axios.post('/boards/writeform', form, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
       });
-
-      try {
-        await axios.post('/boards/writeform', form, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-        });
-        window.location.href = `/board/${board}/1`;
-      } catch (e: any) {
-        console.error('Error: ', e.message);
-        if (e.response.data.message.startsWith('Validation failed')) {
-          const errorMessage = e.response.data.errors[0].defaultMessage;
-          alert(errorMessage);
-        }
+      window.location.href = `/board/${board}/1`;
+    } catch (e: any) {
+      console.error('에러: ', e.message);
+      if (e.response.data.message.startsWith('Validation failed')) {
+        const errorMessage = e.response.data.errors[0].defaultMessage;
+        alert(errorMessage);
       }
-
+    }
   };
 
   return (
-    <>
-      <SubHeader kind="board" />
-      <Container>
-        {(() => {
-          switch (board) {
-            case 'notice':
-              return <PageTitle>공지사항</PageTitle>;
-            case 'qna':
-              return <PageTitle>문의하기</PageTitle>;
-            case 'review':
-              return <PageTitle>이용후기</PageTitle>;
-            default:
-              return <PageTitle>고객지원</PageTitle>;
-          }
-        })()}
-        <div>
-          <form onSubmit={handleSubmit} encType="multipart/form-data">
-            <Table className="horizontal">
-              <tr>
-                <th style={{ width: '240px' }}>제목</th>
-                <td>
-                  <input type="text" className="title long" name="title" value={formData.title} onChange={handleChange} required />
-                </td>
-              </tr>
-              <tr>
-                <th style={{ width: '240px' }}>카테고리</th>
-                <td>
-                  {(() => {
-                    switch (board) {
-                      case 'qna':
-                        return (
-                          <select name="category" onChange={handleChange}>
-                            <option value="문의1">문의1</option>
-                            <option value="문의2">문의2</option>
-                          </select>
-                        );
-                      case 'review':
-                        return (
-                          <select name="category" onChange={handleChange}>
-                            <option value="객실">객실</option>
-                            <option value="다이닝">다이닝</option>
-                          </select>
-                        );
-                      default:
-                        return;
-                    }
-                  })()}
-                </td>
-              </tr>
-              <tr>
-                <th>작성자</th>
-                <td>
-                  <input type="text" name="boardWriter" value={formData.boardWriter} onChange={handleChange} required />
-                </td>
-              </tr>
-              <tr className="contents">
-                <td colSpan={2} className="writeWrapper">
-                  <TextEditor setValue={setBoardContent} />
-                </td>
-              </tr>
-              <tr className="conbtm">
-                <th>첨부파일</th>
-                <td>
-                  <input type="file" accept="image/*" onChange={saveImgFile} ref={imgRef} />
-                  {imgFile ? <img src={imgFile} alt="후기 이미지" /> : <img style={{ display: 'none' }} />}
-                </td>
-              </tr>
-            </Table>
-            <BtnWrapper className="center double mt40">
-              <SubmitBtn type="submit">작성하기</SubmitBtn>
-              <SubmitBtn type="button" onClick={() => navigate(-1)}>
-                취소
-              </SubmitBtn>
-            </BtnWrapper>
-          </form>
-        </div>
-      </Container>
-    </>
+      <>
+        <SubHeader kind="board" />
+        <Container>
+          {(() => {
+            switch (board) {
+              case 'notice':
+                return <PageTitle>공지사항</PageTitle>;
+              case 'qna':
+                return <PageTitle>문의하기</PageTitle>;
+              case 'review':
+                return <PageTitle>이용후기</PageTitle>;
+              default:
+                return <PageTitle>고객지원</PageTitle>;
+            }
+          })()}
+          <div>
+            <form onSubmit={handleSubmit} encType="multipart/form-data">
+              <Table className="horizontal">
+                <tr>
+                  <th style={{ width: '240px' }}>제목</th>
+                  <td>
+                    <input type="text" className="title long" name="title" value={formData.title} onChange={handleChange} required />
+                  </td>
+                </tr>
+                <tr>
+                  <th style={{ width: '240px' }}>카테고리</th>
+                  <td>
+                    {(() => {
+                      switch (board) {
+                        case 'qna':
+                          return (
+                              <select name="category" onChange={handleChange}>
+                                <option value="">선택</option>
+                                <option value="문의1">문의1</option>
+                                <option value="문의2">문의2</option>
+                              </select>
+                          );
+                        case 'review':
+                          return (
+                              <select name="category" onChange={handleChange}>
+                                <option value="">선택</option>
+                                <option value="객실">객실</option>
+                                <option value="다이닝">다이닝</option>
+                              </select>
+                          );
+                        default:
+                          return;
+                      }
+                    })()}
+                    {categoryError && <div style={{ color: 'red' }}>{categoryError}</div>}
+                  </td>
+                </tr>
+                <tr>
+                  <th>작성자</th>
+                  <td>
+                    <input type="text" name="boardWriter" value={formData.boardWriter} onChange={handleChange} required />
+                  </td>
+                </tr>
+                <tr className="contents">
+                  <td colSpan={2} className="writeWrapper">
+                    <TextEditor setValue={setBoardContent} />
+                  </td>
+                </tr>
+                <tr className="conbtm">
+                  <th>첨부파일</th>
+                  <td>
+                    <input type="file" accept="image/*" onChange={saveImgFile} ref={imgRef} />
+                    {imgFile ? <img src={imgFile} alt="후기 이미지" /> : <img style={{ display: 'none' }} />}
+                  </td>
+                </tr>
+              </Table>
+              <BtnWrapper className="center double mt40">
+                <SubmitBtn type="submit">작성하기</SubmitBtn>
+                <SubmitBtn type="button" onClick={() => navigate(-1)}>
+                  취소
+                </SubmitBtn>
+              </BtnWrapper>
+            </form>
+          </div>
+        </Container>
+      </>
   );
 };
 
