2cbf747bad79d75f3ee3dd145f3c5cb1fce6fbb4
Sohee Moon <soheetech@gmail.com>
2023-10-08 11:05:54 +0900

    스타일 분리 완료

--- frontend/src/pages/board/BoardList.tsx
+++ frontend/src/pages/board/BoardList.tsx
@@ -1,69 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { styled } from 'styled-components';
+import * as S from './Style';
 import { useParams } from 'react-router-dom'; // Remove duplicate import
-import { commonContainerStyle, PageTitle, BtnWrapper, LinkBtn, commonTable } from '../../Style/commonStyles';
+import { PageTitle, LinkBtn } from '../../Style/commonStyles';
 import SubHeader from '../../components/layout/SubHeader/SubHeader';
 import Paging from '../../components/common/Paging/Paging';
 import axios from 'axios';
 import Instance from '../../utils/api/axiosInstance';
 import Search from '../../components/common/Search/Search';
 import { Link } from 'react-router-dom';
-
-export const Container = styled(commonContainerStyle)``;
-
-const IsReply = styled.span`
-  display: inline-block;
-  background: ${(props) => props.theme.colors.gold};
-  color: white;
-  font-size: ${(props) => props.theme.font.sizexxxs};
-  padding: 0 7px;
-  border-radius: 8px;
-  height: 14px;
-  line-height: 14px;
-  margin-right: 10px;
-  vertical-align: middle;
-`;
-
-const BoardGallery = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 40px 20px;
-
-  li {
-    width: 380px;
-    line-height: 1.4;
-  }
-
-  li .thumbnail {
-    background: ${(props) => props.theme.colors.graybg};
-    height: 240px;
-    margin-bottom: 16px;
-
-    img {
-      min-width: 100%;
-      max-width: 100%;
-      min-height: 100%;
-      max-height: 100%;
-      object-fit: cover;
-    }
-  }
-
-  li .writer {
-    margin: 6px 0 2px;
-  }
-  li .writer,
-  li .date {
-    font-size: ${(props) => props.theme.font.sizexs};
-    color: ${(props) => props.theme.colors.graylight};
-  }
-`;
-
-const WriteBtnWrapper = styled(BtnWrapper)`
-  margin-top: -70px;
-  margin-bottom: 20px;
-`;
-
-const Table = styled(commonTable)``;
 
 const CustomerSupport = () => {
   const board = useParams().board;
@@ -101,34 +45,32 @@ const CustomerSupport = () => {
   }, [board]);
 
   useEffect(() => {
-    if(board === 'review'){
+    if (board === 'review') {
       boards.map((board) => {
         GetImageUrl(board.boardId);
       });
     }
   }, [boards]);
 
-  const GetImageUrl = (boardId:number) => {
+  const GetImageUrl = (boardId: number) => {
     Instance.get(`/boards/image/${boardId}`, {
       responseType: 'arraybuffer',
     })
-        .then((response) => {
-          if (response.status === 200) {
-            const blob = new Blob([response.data], {
-              type: response.headers['content-type'],
-            });
-            let image = { boardId: boardId, imageUrl: URL.createObjectURL(blob) };
-            setImageUrl((prevImages) => [...prevImages, image]);
-          } else if (response.status === 204) {
-            console.log('해당 boardId가 없음 : ', boardId);
-          } else {
-            console.error('요청 에러 : ', response.status);
-          }
-        })
-        .catch((error) => {
-        });
+      .then((response) => {
+        if (response.status === 200) {
+          const blob = new Blob([response.data], {
+            type: response.headers['content-type'],
+          });
+          let image = { boardId: boardId, imageUrl: URL.createObjectURL(blob) };
+          setImageUrl((prevImages) => [...prevImages, image]);
+        } else if (response.status === 204) {
+          console.log('해당 boardId가 없음 : ', boardId);
+        } else {
+          console.error('요청 에러 : ', response.status);
+        }
+      })
+      .catch((error) => {});
   };
-
 
   let writeDate;
   let newMonth;
@@ -142,119 +84,113 @@ const CustomerSupport = () => {
   });
 
   return (
-      <>
-        <SubHeader kind="board" />
-        <Container>
+    <>
+      <SubHeader kind="board" />
+      <S.Container>
+        {(() => {
+          switch (board) {
+            case 'notice':
+              return <PageTitle key="notice">공지사항</PageTitle>;
+            case 'qna':
+              return <PageTitle key="qna">문의하기</PageTitle>;
+            case 'review':
+              return <PageTitle key="review">이용후기</PageTitle>;
+            default:
+              return <PageTitle key="default">고객지원</PageTitle>;
+          }
+        })()}
+        <div>
           {(() => {
-            switch (board) {
-              case 'notice':
-                return <PageTitle key="notice">공지사항</PageTitle>;
-              case 'qna':
-                return <PageTitle key="qna">문의하기</PageTitle>;
-              case 'review':
-                return <PageTitle key="review">이용후기</PageTitle>;
-              default:
-                return <PageTitle key="default">고객지원</PageTitle>;
+            if (board !== 'notice') {
+              return (
+                <S.WriteBtnWrapper className="right">
+                  <LinkBtn to={`/board/` + board + `/write`}>작성하기</LinkBtn>
+                </S.WriteBtnWrapper>
+              );
             }
           })()}
-          <div>
-            {(() => {
-              if (board !== 'notice') {
-                return (
-                    <WriteBtnWrapper className="right">
-                      <LinkBtn to={`/board/` + board + `/write`}>작성하기</LinkBtn>
-                    </WriteBtnWrapper>
-                );
-              }
-            })()}
-            {(() => {
-              if (board === 'review') {
-                return (
-                    <BoardGallery>
-                      {/** loop */}
-                      {boards.length === 0 && (
-                          <li key="empty" className="empty">
-                            <p>작성된 게시글이 없습니다.</p>
-                          </li>
-                      )}
-                      {boards && boards.map((item) => (
-                          <li key={item.boardId}>
-                            <div className="thumbnail">
-                              <Link to={`/board/${board}/detail/${item.boardId}`}>
-                                {imageUrl.find((image) => image.boardId === item.boardId) && (
-                                    <img
-                                        src={imageUrl.find((image) => image.boardId === item.boardId).imageUrl}
-                                        alt={`Image for ${item.title}`}
-                                    />
-                                )}
-                              </Link>
-                            </div>
-                            <p className="title">
-                              <Link to={`/board/${board}/detail/${item.boardId}`}>{item.title}</Link>
-                            </p>
-                            <p className="writer">{item.boardWriter}</p>
-                            <p className="date">{`${item.boardWriteDate[0]}.${item.boardWriteDate[1] < 10 ? '0' : ''}${
-                                item.boardWriteDate[1]
-                            }.${
-                                item.boardWriteDate[2] < 10 ? '0' : ''
-                            }${item.boardWriteDate[2]}`}</p>
-                          </li>
-                      ))}
-                      {/** // loop */}
-                    </BoardGallery>
-                );
-              } else {
-                return (
-                    <Table className="userpage">
-                      <colgroup>
-                        <col width="110px" />
-                        <col width="180px" />
-                        <col width="auto" />
-                        <col width="180px" />
-                      </colgroup>
-                      <thead>
-                      <tr>
-                        <th>번호</th>
-                        <th>카테고리</th>
-                        <th>제목</th>
-                        <th>작성일</th>
+          {(() => {
+            if (board === 'review') {
+              return (
+                <S.BoardGallery>
+                  {/** loop */}
+                  {boards.length === 0 && (
+                    <li key="empty" className="empty">
+                      <p>작성된 게시글이 없습니다.</p>
+                    </li>
+                  )}
+                  {boards &&
+                    boards.map((item) => (
+                      <li key={item.boardId}>
+                        <div className="thumbnail">
+                          <Link to={`/board/${board}/detail/${item.boardId}`}>
+                            {imageUrl.find((image) => image.boardId === item.boardId) && (
+                              <img src={imageUrl.find((image) => image.boardId === item.boardId).imageUrl} alt={`Image for ${item.title}`} />
+                            )}
+                          </Link>
+                        </div>
+                        <p className="title">
+                          <Link to={`/board/${board}/detail/${item.boardId}`}>{item.title}</Link>
+                        </p>
+                        <p className="writer">{item.boardWriter}</p>
+                        <p className="date">{`${item.boardWriteDate[0]}.${item.boardWriteDate[1] < 10 ? '0' : ''}${item.boardWriteDate[1]}.${
+                          item.boardWriteDate[2] < 10 ? '0' : ''
+                        }${item.boardWriteDate[2]}`}</p>
+                      </li>
+                    ))}
+                  {/** // loop */}
+                </S.BoardGallery>
+              );
+            } else {
+              return (
+                <S.Table className="userpage">
+                  <colgroup>
+                    <col width="110px" />
+                    <col width="180px" />
+                    <col width="auto" />
+                    <col width="180px" />
+                  </colgroup>
+                  <thead>
+                    <tr>
+                      <th>번호</th>
+                      <th>카테고리</th>
+                      <th>제목</th>
+                      <th>작성일</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {boards.length === 0 && (
+                      <tr key="no-data">
+                        <td colSpan={7} className="center">
+                          작성된 게시글이 없습니다.
+                        </td>
                       </tr>
-                      </thead>
-                      <tbody>
-                      {boards.length === 0 && (
-                          <tr key="no-data">
-                            <td colSpan={7} className="center">
-                              작성된 게시글이 없습니다.
-                            </td>
-                          </tr>
-                      )}
-                      {/** loop */}
-                      {boards.map((item, index) => (
-                          <tr key={item.boardId}>
-                            <td className="center">{totalData - index}</td>
-                            <td className="center">{item.category}</td>
-                            <td>
-                              {/* <IsReply>답글</IsReply> */}
-                              {/** 답글 여부에 따라 보이거나 안 보이게 처리 */}
-                              <Link to={`/board/${board}/detail/${item.boardId}`}>{item.title}</Link>
-                            </td>
-                            <td className="center">{`${item.boardWriteDate[0]}.${item.boardWriteDate[1] < 10 ? '0' : ''}${
-                                item.boardWriteDate[1]
-                            }.${
-                                item.boardWriteDate[2] < 10 ? '0' : ''
-                            }${item.boardWriteDate[2]}`}</td>
-                          </tr>
-                      ))}
-                      {/** // loop */}
-                      </tbody>
-                    </Table>
-                );
-              }
-            })()}
-          </div>
-          <Paging totalPage={totalPages} />
-        </Container>
-      </>
+                    )}
+                    {/** loop */}
+                    {boards.map((item, index) => (
+                      <tr key={item.boardId}>
+                        <td className="center">{totalData - index}</td>
+                        <td className="center">{item.category}</td>
+                        <td>
+                          {/* <IsReply>답글</IsReply> */}
+                          {/** 답글 여부에 따라 보이거나 안 보이게 처리 */}
+                          <Link to={`/board/${board}/detail/${item.boardId}`}>{item.title}</Link>
+                        </td>
+                        <td className="center">{`${item.boardWriteDate[0]}.${item.boardWriteDate[1] < 10 ? '0' : ''}${item.boardWriteDate[1]}.${
+                          item.boardWriteDate[2] < 10 ? '0' : ''
+                        }${item.boardWriteDate[2]}`}</td>
+                      </tr>
+                    ))}
+                    {/** // loop */}
+                  </tbody>
+                </S.Table>
+              );
+            }
+          })()}
+        </div>
+        <Paging totalPage={totalPages} />
+      </S.Container>
+    </>
   );
 };
 
---
+++ frontend/src/pages/board/Style.ts
@@ -0,0 +1,124 @@
+import styled from 'styled-components';
+import { commonContainerStyle, BtnWrapper, commonTable } from '../../Style/commonStyles';
+
+export const Container = styled(commonContainerStyle)``;
+
+export const IsReply = styled.span`
+  // 답글 아이콘
+  display: inline-block;
+  background: ${(props) => props.theme.colors.gold};
+  color: white;
+  font-size: ${(props) => props.theme.font.sizexxxs};
+  padding: 0 7px;
+  border-radius: 8px;
+  height: 14px;
+  line-height: 14px;
+  margin-right: 10px;
+  vertical-align: middle;
+`;
+
+export const BoardGallery = styled.ul`
+  // 갤러리형 게시판 리스트 스타일
+  display: flex;
+  flex-wrap: wrap;
+  gap: 40px 20px;
+
+  li {
+    width: 380px;
+    line-height: 1.4;
+
+    &.empty {
+      text-align: center;
+      width: 100%;
+      padding: 150px 0;
+    }
+  }
+
+  li .thumbnail {
+    background: ${(props) => props.theme.colors.graybg};
+    height: 240px;
+    margin-bottom: 16px;
+
+    img {
+      min-width: 100%;
+      max-width: 100%;
+      min-height: 100%;
+      max-height: 100%;
+      object-fit: cover;
+    }
+  }
+
+  li .writer {
+    margin: 6px 0 2px;
+  }
+  li .writer,
+  li .date {
+    font-size: ${(props) => props.theme.font.sizexs};
+    color: ${(props) => props.theme.colors.graylight};
+  }
+`;
+
+export const WriteBtnWrapper = styled(BtnWrapper)`
+  // 리스트 페이지 작성하기 버튼 wrapper
+  margin-top: -70px;
+  margin-bottom: 20px;
+`;
+
+export const Table = styled(commonTable)``;
+
+export const TableRead = styled.table`
+  // 게시판 상세 스타일
+  border-bottom: 1px solid ${(props) => props.theme.colors.charcoal};
+  width: 100%;
+
+  th {
+    border-top: 1px solid ${(props) => props.theme.colors.graylightborder};
+    font-weight: 500;
+    background: ${(props) => props.theme.colors.graybg};
+    color: ${(props) => props.theme.colors.charcoal};
+    vertical-align: top;
+  }
+  th,
+  td {
+    padding: 40px;
+  }
+  td {
+    border-top: 1px solid ${(props) => props.theme.colors.graylightborder};
+    color: ${(props) => props.theme.colors.graydark};
+  }
+  .contents textarea {
+    width: 100%;
+    min-height: 300px;
+    resize: none;
+    border: 0;
+  }
+  tr:first-child th,
+  tr:first-child td {
+    border-top-color: ${(props) => props.theme.colors.charcoal};
+  }
+  input {
+    height: 36px;
+  }
+  input[type='file'] {
+    padding: 0;
+    border: 0;
+  }
+  input.title {
+    width: 80%;
+  }
+  td.titlew p {
+    font-size: ${(props) => props.theme.font.sizes};
+  }
+  td.titlew p span {
+    margin-right: 8px;
+    color: ${(props) => props.theme.colors.graylight};
+  }
+  td.titlew .title {
+    font-size: ${(props) => props.theme.font.sizesl};
+    color: ${(props) => props.theme.colors.charcoal};
+    margin-bottom: 14px;
+  }
+  td.titlew {
+    background: ${(props) => props.theme.colors.graybg};
+  }
+`;
