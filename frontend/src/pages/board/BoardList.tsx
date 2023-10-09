3d8947c6ed4c5413a6487c9c97591dd2dc2d4aa0
LEE-Donggyu <ehdrb141@naver.com>
2023-10-09 16:41:26 +0900

    .

--- frontend/src/pages/board/BoardList.tsx
+++ frontend/src/pages/board/BoardList.tsx
@@ -224,7 +224,8 @@ const CustomerSupport = () => {
                             <td>
                               {/* <IsReply>답글</IsReply> */}
                               {/** 답글 여부에 따라 보이거나 안 보이게 처리 */}
-                                <Link to={`/board/${board}/detail/${item.title}?boardId=${item.boardId}`}>{item.title}</Link>
+                                <Link
+                                    to={{pathname: `/board/${board}/detail/${item.title}`,search: `boardId=${item.boardId}`}}>{item.title}</Link>
                             </td>
                             <td className="center">{`${item.boardWriteDate[0]}.${item.boardWriteDate[1] < 10 ? '0' : ''}${
                                 item.boardWriteDate[1]
