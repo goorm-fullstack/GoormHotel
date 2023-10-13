59b40e95c1af6690e31ff43585471d4049b1d55d
WhiteKIM <9rudrb40@naver.com>
2023-10-13 12:06:48 +0900

    2023.10.13 upload
    운영자 관리 업데이트, 쿠키 인코딩 추가

--- backend/src/main/java/goormknights/hotel/HotelApplication.java
+++ backend/src/main/java/goormknights/hotel/HotelApplication.java
@@ -1,6 +1,8 @@
 package goormknights.hotel;
 
 import goormknights.hotel.global.entity.Role;
+import goormknights.hotel.item.model.Dining;
+import goormknights.hotel.item.repository.DiningRepository;
 import goormknights.hotel.member.model.Member;
 import goormknights.hotel.member.repository.MemberRepository;
 import jakarta.annotation.PostConstruct;
@@ -18,6 +20,7 @@ import java.time.LocalDate;
 @EnableJpaAuditing
 @RequiredArgsConstructor
 public class HotelApplication {
+	private final DiningRepository diningRepository;
 	private final MemberRepository memberRepository;
 	private final PasswordEncoder passwordEncoder;
 
@@ -25,21 +28,35 @@ public class HotelApplication {
 		SpringApplication.run(HotelApplication.class, args);
 	}
 
-//	@PostConstruct
-//	void init() {
-//		Member member = Member.builder()
-//				.memberId("test")
-//				.email("test@test.com")
-//				.grade("Bronze")
-//				.password(passwordEncoder.encode("1234"))
-//				.name("test")
-//				.phoneNumber("test")
-//				.birth(LocalDate.now())
-//				.gender("M")
-//				.mailAuth(true)
-//				.role(Role.USER)
-//				.build();
-//		memberRepository.save(member);
-//
-//	}
+	@PostConstruct
+	void init() {
+		Dining dining = Dining.builder()
+				.name("test")
+				.price(1000)
+				.capacity(4)
+				.description("test")
+				.priceAdult(1000)
+				.priceChildren(100)
+				.spare(4)
+				.spareAdult(1)
+				.spareChildren(1)
+				.type("dining")
+				.typeDetail("dining")
+				.build();
+
+		Member member = Member.builder()
+				.memberId("test")
+				.email("test@test.com")
+				.grade("Bronze")
+				.password(passwordEncoder.encode("1234"))
+				.name("test")
+				.phoneNumber("test")
+				.birth(LocalDate.now())
+				.gender("M")
+				.mailAuth(true)
+				.role(Role.USER)
+				.build();
+		memberRepository.save(member);
+		diningRepository.save(dining);
+	}
 }
