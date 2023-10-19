b20ce1d181ddf490dd75f4df01baa3e9f5a776d7
jinhwan Bea <paewhal20@gmail.com>
2023-10-20 01:51:23 +0900

    객실수, 좌석수 표시 안되는 문제 해결

--- frontend/src/pages/reservation/ReservationPage.tsx
+++ frontend/src/pages/reservation/ReservationPage.tsx
@@ -43,15 +43,15 @@ interface Coupon {
 const ReservationPage = () => {
   const [giftCardNumber, setGiftCardNumber] = useState('');
   const [memberData, setMemberData] = useState();
-  const userLoggedIn = localStorage.getItem("memberId");
+  const userLoggedIn = localStorage.getItem('memberId');
   const location = useLocation();
   const { reservationData, selectedProduct, selectData, indexImg } = location.state;
   const navigate = useNavigate();
   const [nights, setNights] = useState(1);
   const [giftcardList, setGiftCardList] = useState<GiftCard[]>([]);
   const [couponList, setCouponList] = useState<Coupon[]>([]);
-  const [selectCoupon, setSelectCoupon] = useState('');//적용할 쿠폰
-  const [selectGiftCard, setSelectGiftCard] = useState<number[]>([]);//적용할 상품권
+  const [selectCoupon, setSelectCoupon] = useState(''); //적용할 쿠폰
+  const [selectGiftCard, setSelectGiftCard] = useState<number[]>([]); //적용할 상품권
   const [click, setClick] = useState(false);
   const [memberId, setMemberId] = useState(0);
   const [selectAllChecked, setSelectAllChecked] = useState(false);
@@ -59,7 +59,7 @@ const ReservationPage = () => {
   const [email, setEmail] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [notice, setNotice] = useState('');
-  const [isLogined, setIsLogined] = useState(false); 
+  const [isLogined, setIsLogined] = useState(false);
 
   const [formData, setFormData] = useState({
     checkIn: reservationData?.checkInDate || '',
@@ -78,16 +78,16 @@ const ReservationPage = () => {
 
   //member데이터를 불러오는 로직
   useEffect(() => {
-    if(userLoggedIn) {
+    if (userLoggedIn) {
       const params = {
-      id: localStorage.getItem('memberId'),
+        id: localStorage.getItem('memberId'),
       };
       Instance.get('/member/find', { params }).then((response) => {
         setMemberData(response.data);
         setCouponList(response.data.couponList);
         setMemberId(response.data.id);
       });
-      setIsLogined(true)
+      setIsLogined(true);
     } else {
       setIsLogined(false);
     }
@@ -109,7 +109,7 @@ const ReservationPage = () => {
     Instance.post(`/api/giftcard/register?&uuid=${giftCardNumber}`).then((response) => {
       if (response.status === 200) {
         const registerGiftCard = response.data;
-        setGiftCardList((g) => [...g, registerGiftCard])
+        setGiftCardList((g) => [...g, registerGiftCard]);
         setClick(true);
       }
     });
@@ -134,12 +134,12 @@ const ReservationPage = () => {
       checkOut: formatDateForServer(formData.checkOut),
     };
 
-    if(isLogined) {
+    if (isLogined) {
       if (confirmed) {
         try {
           await Instance.post(`/reservation/save`, {
-            params : memberId,
-            data : serverFormattedData
+            params: memberId,
+            data: serverFormattedData,
           });
           navigate('/');
           window.alert('예약이 완료되었습니다');
@@ -148,18 +148,18 @@ const ReservationPage = () => {
         }
       }
     } else {
-      if(window.confirm('로그인되지 않았습니다. 비회원으로 예약을 진행하시겠습니까?')) {
+      if (window.confirm('로그인되지 않았습니다. 비회원으로 예약을 진행하시겠습니까?')) {
         try {
           let anonymousSignupDto = {
-            name : name,
-            email : email,
-            phoneNumber : phoneNumber
+            name: name,
+            email: email,
+            phoneNumber: phoneNumber,
           };
           const serverFormattedData = {
             ...formData,
-            checkIn: formatDateForServer(new Date(2023,11,25,3,50)),//테스트 데이터
-            checkOut: formatDateForServer(new Date(2023,11,25,3,50)),
-            anonymousSignupDto : anonymousSignupDto
+            checkIn: formatDateForServer(new Date(2023, 11, 25, 3, 50)), //테스트 데이터
+            checkOut: formatDateForServer(new Date(2023, 11, 25, 3, 50)),
+            anonymousSignupDto: anonymousSignupDto,
           };
           await Instance.post(`/reservation/save/anonymous`, serverFormattedData);
           navigate('/');
@@ -175,7 +175,7 @@ const ReservationPage = () => {
     setFormData(newData);
   };
 
-  const handleSelectGiftCardAllChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+  const handleSelectGiftCardAllChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const checked = e.target.checked;
     setSelectAllChecked(checked);
     if (checked) {
@@ -186,7 +186,7 @@ const ReservationPage = () => {
     }
   };
 
-  const handleGiftCardCheckboxChange = (id : number) => {
+  const handleGiftCardCheckboxChange = (id: number) => {
     setSelectGiftCard((prevItems) => {
       if (prevItems.includes(id)) {
         return prevItems.filter((item) => item !== id);
@@ -205,16 +205,26 @@ const ReservationPage = () => {
             <S.Section>
               <ContentsTitleXSmall>상품 상세 설정</ContentsTitleXSmall>
               <S.OptionWrap>
-                <Reservation updateReservationData={updateReservationData} selectedProduct={selectedProduct} />
+                <Reservation
+                  updateReservationData={updateReservationData}
+                  selectedProduct={selectedProduct !== undefined ? selectedProduct : selectData}
+                />
               </S.OptionWrap>
             </S.Section>
 
             <S.Section className="privacy">
               <ContentsTitleXSmall>고객 정보 입력</ContentsTitleXSmall>
               <div className="flex">
-                <input placeholder="고객명" type="text" value={name} onChange={(e) => setName(e.target.value)} required readOnly={isLogined}/>
-                <input placeholder="연락처" type="text" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} required readOnly={isLogined}/>
-                <input placeholder="이메일" type="text" value={email} onChange={(e) => setEmail(e.target.value)} required readOnly={isLogined}/>
+                <input placeholder="고객명" type="text" value={name} onChange={(e) => setName(e.target.value)} required readOnly={isLogined} />
+                <input
+                  placeholder="연락처"
+                  type="text"
+                  value={phoneNumber}
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                  required
+                  readOnly={isLogined}
+                />
+                <input placeholder="이메일" type="text" value={email} onChange={(e) => setEmail(e.target.value)} required readOnly={isLogined} />
               </div>
               <div className="full">
                 <input placeholder="요청사항" type="text" value={notice} onChange={(e) => setNotice(e.target.value)} />
@@ -231,7 +241,7 @@ const ReservationPage = () => {
                 <BtnWrapper className="flexspace">
                   <div>
                     <CheckLabel>
-                      <InputCheckbox type="checkbox" onChange={handleSelectGiftCardAllChange}/> 전체 선택
+                      <InputCheckbox type="checkbox" onChange={handleSelectGiftCardAllChange} /> 전체 선택
                     </CheckLabel>
                   </div>
                   <BtnWrapper className="flexgap">
@@ -243,22 +253,25 @@ const ReservationPage = () => {
                     </NormalBtn>
                   </BtnWrapper>
                 </BtnWrapper>
-                <table>{/* 상품권 목록 출력하는 곳*/}
+                <table>
+                  {/* 상품권 목록 출력하는 곳*/}
                   {giftcardList.length !== 0 ? (
                     <>
                       {giftcardList.map((giftcard, index) => (
-                      <tr key={index}>
-                      <td>
-                        <CheckLabel>
-                          <InputCheckbox type="checkbox"
-                          checked={selectGiftCard.includes(giftcard.id)}
-                          onChange={() => handleGiftCardCheckboxChange(giftcard.id)}/>
-                          {giftcard.title}
-                        </CheckLabel>
-                      </td>
-                        <td className="right">{giftcard.money}</td>
-                      </tr>
-                  ))}
+                        <tr key={index}>
+                          <td>
+                            <CheckLabel>
+                              <InputCheckbox
+                                type="checkbox"
+                                checked={selectGiftCard.includes(giftcard.id)}
+                                onChange={() => handleGiftCardCheckboxChange(giftcard.id)}
+                              />
+                              {giftcard.title}
+                            </CheckLabel>
+                          </td>
+                          <td className="right">{giftcard.money}</td>
+                        </tr>
+                      ))}
                     </>
                   ) : (
                     <tr>
@@ -322,7 +335,11 @@ const ReservationPage = () => {
 
           <S.Right>
             <ContentsTitleXSmall>상품 개요</ContentsTitleXSmall>
-            {indexImg ? <Item selectedProduct={selectedProduct ? selectedProduct : selectData} indexImg={indexImg} /> : <Item selectedProduct={selectedProduct ? selectedProduct : selectData} />}
+            {indexImg ? (
+              <Item selectedProduct={selectedProduct ? selectedProduct : selectData} indexImg={indexImg} />
+            ) : (
+              <Item selectedProduct={selectedProduct ? selectedProduct : selectData} />
+            )}
             <BtnWrapper className="full mt20">
               <SubmitBtn type="submit" className="shadow" onClick={handleReservation}>
                 예약 및 결제하기
