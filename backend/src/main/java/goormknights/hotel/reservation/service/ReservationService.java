8fa38a164929589b585b1116655327f406e21bf6
jinhwan Bea <paewhal20@gmail.com>
2023-10-26 10:05:56 +0900

    Update ReservationService.java

--- backend/src/main/java/goormknights/hotel/reservation/service/ReservationService.java
+++ backend/src/main/java/goormknights/hotel/reservation/service/ReservationService.java
@@ -38,7 +38,7 @@ public class ReservationService {
      *
      * @param reservationDto - user가 입력한 정보
      */
-    public void saveReservation(RequestReservationDto reservationDto) throws Throwable {
+    public void saveReservation(RequestReservationDto reservationDto) {
 
         // 상품 정보 세팅
 //        Optional<Item> itemInfo = itemRepository.findById(reservationDto.getItemId());
